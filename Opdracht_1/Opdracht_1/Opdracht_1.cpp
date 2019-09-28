d6dea8d6764a23a953888ae17949446b3a1e7aaa
PitsCrafts <48951566+PitsCrafts@users.noreply.github.com>
2019-09-28 15:22:54 +0200

    Add files via upload

---
+++ Opdracht_1/Opdracht_1/Opdracht_1.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+//Objecten(atributten): Module(naam, ec), Docent(naam, salaris), Student(naam, studentnr)
+//Docent beinvloed Module (zonder docent geen module), Module beinvloed Student (zonder module geen ec voor student)
